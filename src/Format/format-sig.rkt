5acfc5e39f7f537b5ab37f26edb973555e405439
NoahStoryM <noahstorym@gmail.com>
2021-11-09 17:34:43 +0800

    Fix format-sig.

--- src/Format/format-sig.rkt
+++ src/Format/format-sig.rkt
@@ -1,12 +1,21 @@
 #lang typed/racket/base
 
-(require typed/racket/unit)
+(require typed/racket/unit
+         "../types.rkt")
 
 (provide format^)
 
 
 (define-signature format^
   (
-   [format-json : (case-> [-> (U String Bytes) #:type 'string [#:indent String] String]
-                          [-> (U String Bytes) #:type 'bytes  [#:indent String] Bytes ])]
+   [format-json : (case-> [-> (U String Bytes)
+                              #:type 'string
+                              [#:encode Encode]
+                              [#:indent String]
+                              String]
+                          [-> (U String Bytes)
+                              #:type 'bytes
+                              [#:encode Encode]
+                              [#:indent String]
+                              Bytes])]
    ))
