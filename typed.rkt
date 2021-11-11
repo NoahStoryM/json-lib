eb89721d62e572e42bd55b5bc346758f20f7e153
NoahStoryM <noahstorym@gmail.com>
2021-11-11 18:44:20 +0800

    Regard mutable-json? and json? as predicates.

--- typed.rkt
+++ typed.rkt
@@ -4,8 +4,8 @@
 
 (require "src/main.rkt")
 (unsafe-require/typed "untyped.rkt"
-  [mutable-json? [-> Any Boolean]]
-  [json?         [-> Any Boolean]])
+  [mutable-json? [-> Any Boolean : Mutable-JSON]]
+  [json?         [-> Any Boolean : JSON]])
 
 (provide (all-from-out "src/main.rkt")
          mutable-json?
