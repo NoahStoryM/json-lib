d3333bc2ffc9711caed9d12ecd818d2e7d61ce15
NoahStoryM <noahstorym@gmail.com>
2021-11-14 15:36:01 +0800

    define json? by typed/racket.

--- typed.rkt
+++ typed.rkt
@@ -4,9 +4,13 @@
 
 (require "src/main.rkt")
 (unsafe-require/typed "untyped.rkt"
-  [mutable-json? [-> Any Boolean : Mutable-JSON]]
-  [json?         [-> Any Boolean : JSON]])
+  [mutable-json? [-> Any Boolean : Mutable-JSON]])
 
 (provide (all-from-out "src/main.rkt")
          mutable-json?
          json?)
+
+(: json? [-> Any Boolean : JSON])
+(define (json? arg)
+  (or (immutable-json? arg)
+      (mutable-json? arg)))
