6c6d595e88751f998097c56386c987af986ae04a
NoahStoryM <noahstorym@gmail.com>
2022-06-11 23:01:05 +0800

    Update format style.

--- src/typed-help.rkt
+++ src/typed-help.rkt
@@ -1,23 +1,16 @@
 #lang typed/racket/base
 
-(provide mmap
-         mmap->list
-         map->mlist
-         ;; andmmap
-         ;; ormmap
-         mreverse
-         list->mlist
-         mlist->list)
+(require typed/racket/unsafe)
 
+(unsafe-require/typed/provide compatibility/mlist
+  [mlist (All (A) [-> A * (MListof A)])]
+  [mmap (All (A B) [-> [-> A B] (MListof A) (MListof B)])]
+  [mlength [-> (U Null MPairTop) Index]]
+  [mreverse (All (A) [-> (MListof A) (MListof A)])]
+  [list->mlist (All (A) [-> (Listof A) (MListof A)])]
+  [mlist->list (All (A) [-> (MListof A) (Listof A)])])
 
-(: id (All (A) [-> A A]))
-(define id (λ (arg) arg))
-
-
-(: mmap (All (A B) [-> [-> A B] (MListof A) (MListof B)]))
-(define (mmap f l)
-  (let loop ([l l] [res : (MListof B) '()])
-    (if (null? l) (mreverse res) (loop (mcdr l) (mcons (f (mcar l)) res)))))
+(provide mmap->list map->mlist #;andmmap #;ormmap)
 
 (: mmap->list (All (A B) [-> [-> A B] (MListof A) (Listof B)]))
 (define (mmap->list f l)
@@ -29,23 +22,13 @@
   (let loop ([l l] [res : (MListof B) '()])
     (if (null? l) (mreverse res) (loop (cdr l) (mcons (f (car l)) res)))))
 
-;; (: andmmap (All (A B) [-> [-> A B] (MListof A) (U True  B)]))
-;; (define (andmmap f l)
-;;   (let loop ([l l])
-;;     (if (null? l) #t (and (f (mcar l)) (loop (mcdr l))))))
+#;(: andmmap (All (A B) [-> [-> A B] (MListof A) (U True  B)]))
+#;(define (andmmap f l)
+    (let loop ([l l])
+      (if (null? l) #t (and (f (mcar l)) (loop (mcdr l))))))
 
-;; (: ormmap  (All (A B) [-> [-> A B] (MListof A) (U False B)]))
-;; (define (ormmap  f l)
-;;   (let loop ([l l])
-;;     (if (null? l) #f (or (f (mcar l)) (loop (mcdr l))))))
+#;(: ormmap  (All (A B) [-> [-> A B] (MListof A) (U False B)]))
+#;(define (ormmap  f l)
+    (let loop ([l l])
+      (if (null? l) #f (or (f (mcar l)) (loop (mcdr l))))))
 
-(: mreverse (All (A) [-> (MListof A) (MListof A)]))
-(define (mreverse l)
-  (let loop ([l l] [res : (MListof A) '()])
-    (if (null? l) res (loop (mcdr l) (mcons (mcar l) res)))))
-
-(: list->mlist (All (A) [-> (Listof A) (MListof A)]))
-(define (list->mlist l) (map->mlist (inst id A) l))
-
-(: mlist->list (All (A) [-> (MListof A) (Listof A)]))
-(define (mlist->list l) (mmap->list (inst id A) l))
